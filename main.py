11852d6b55088d04ad36f51745f23357f1603904
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-23 16:07:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ API_ID = int(26915599)
 API_HASH = "76438a4fa64f9e43b3921a204a79e000"
 BOT_TOKEN = "5895166821:AAFix2nQtVRZqqOtl1QBHw1Uln7hsf_u01M"
 
-
+#Botting Process
 app = Client(name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
 
+#Command Running
+
+@
