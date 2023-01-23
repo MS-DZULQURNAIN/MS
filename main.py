f6d060c1bf00fd6b20f4dca47c4e70e61b20451f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-23 16:05:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ API_HASH = "76438a4fa64f9e43b3921a204a79e000"
 BOT_TOKEN = "5895166821:AAFix2nQtVRZqqOtl1QBHw1Uln7hsf_u01M"
 
 
-app = Client(name="Okk", api_id,
+app = Client(name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
+
