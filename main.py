bdd19975d34c9d6e292fa4dcc60d907b84139639
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-23 16:01:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from pyrogram import Client, filters, idle
+
+API_ID = int(26915599)
+API_HASH = "76438a4fa64f9e43b3921a204a79e000"
+BOT_TOKEN = "5895166821:AAFix2nQtVRZqqOtl1QBHw1Uln7hsf_u01M"
+
+
+app = Client(name="Okk", api_id,
